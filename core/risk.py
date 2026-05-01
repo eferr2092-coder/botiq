a5e1f10041b4c462754f18d32309b8c8de8d2d16
eferr2092-coder <eferr2092@gmail.com>
2026-05-01 18:20:32 -0300

    Update risk.py

--- core/risk.py
+++ core/risk.py
@@ -1,10 +1,2 @@
-lucro_total = 0
-
-def check_stop(valor):
-    global lucro_total
-    lucro_total += valor
-    if lucro_total >= 50:
-        return "STOP WIN"
-    if lucro_total <= -30:
-        return "STOP LOSS"
+def check_stop():
     return None
