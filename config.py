1a6868c3ea1630896c48e59ad3306127791f1f06
eferr2092-coder <eferr2092@gmail.com>
2026-05-01 18:20:49 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,1 @@
-import os
-
-TOKEN = os.getenv("TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
-EMAIL = os.getenv("EMAIL")
-SENHA = os.getenv("SENHA")
-
-VALOR_ENTRADA = float(os.getenv("VALOR_ENTRADA", 10))
-STOP_WIN = float(os.getenv("STOP_WIN", 50))
-STOP_LOSS = float(os.getenv("STOP_LOSS", -30))
+VALOR_ENTRADA = 2
